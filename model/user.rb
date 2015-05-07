025c9c1a3dfae843f6a3cd6f35c2211e7f7df105
Soumya Ray <soumya.ray@gmail.com>
2015-05-08 00:55:17 +0800

    Create authenticate method for User model
    - model/user.rb
    	- def self.authenticate
    	- refactor password hashing into new method: self.hash_password

--- model/user.rb
+++ model/user.rb
@@ -13,13 +13,26 @@ class User < ActiveRecord::Base
 
   def password=(new_password)
     salt = RbNaCl::Random.random_bytes(RbNaCl::PasswordHash::SCrypt::SALTBYTES)
+    digest = self.class.hash_password(salt, new_password)
+    self.salt = Base64.urlsafe_encode64(salt)
+    self.hashed_password = Base64.urlsafe_encode64(digest)
+  end
+
+  def self.authenticate(username, login_password)
+    user = User.find_by_username(username)
+    user && user.password_matches?(login_password) ? user : nil
+  end
+
+  def password_matches?(try_password)
+    salt = Base64.urlsafe_decode64(self.salt)
+    attempted_password = self.class.hash_password(try_password, salt)
+    hashed_password == Base64.urlsafe_encode64(attempted_password)
+  end
+
+  def self.hash_password(pwd, salt)
     opslimit = 2**20
     memlimit = 2**24
     digest_size = 64
-
-    digest = RbNaCl::PasswordHash.scrypt(new_password, salt, opslimit,
-                                         memlimit, digest_size)
-    self.salt = Base64.urlsafe_encode64(salt)
-    self.hashed_password = Base64.urlsafe_encode64(digest )
+    RbNaCl::PasswordHash.scrypt(pwd, salt, opslimit, memlimit, digest_size)
   end
 end
